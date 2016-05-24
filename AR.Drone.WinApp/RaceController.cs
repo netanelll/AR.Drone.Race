a01cf1cb024432c7163ab372c8b0d4cf82f78e7c
soki <sds>
2016-05-24 12:25:59 +0300

    removed unused magnetometer code (going to use tags instead)

--- AR.Drone.WinApp/RaceController.cs
+++ AR.Drone.WinApp/RaceController.cs
@@ -22,11 +22,8 @@ namespace AR.Drone.WinApp
         private bool _isRacing;
         float _x_cord, _y_cord, _z_cord, _roll, _pitch, _yaw;
         const float TICKS_TO_SEC = 0.0000001f; // cov from 100 nano sec to sec
-        public double _north,_northDeg;
         private float _startingYaw; // saving the first yaw response to get the quad direction
-        private int  count;
-        Vector_3 sum = new Vector_3(0,0,0);
-        private double[] _magClib = new double[] { 0.004291144772399, -2.632443996201751e-04, -0.159509130315845, -2.632443995302962e-04, 0.004576539823135, 0.198956637219492 };
+
         #region properties
 
         public float X_cord
@@ -206,53 +203,7 @@ namespace AR.Drone.WinApp
                 _roll = data.Roll;
                 _pitch = data.Pitch;
                 _yaw = data.Yaw - _startingYaw;
-                //   Console.WriteLine(data.Yaw);
-                //  Console.WriteLine(_startingYaw.ToString());
-                //  Console.WriteLine(yaw.ToString());
-                if (count > 100)
-                {
-    //                Vector_3 magnetometer = new Vector_3(data.Magneto.Rectified.X, data.Magneto.Rectified.Y, data.Magneto.Rectified.Z);
-                   // DCM magnetoDCM = new DCM(_roll, _pitch, 0);
-                   DCM magnetoDCM = new DCM(0, 0, 0);
-                    // Vector_3 magnetometer_reltiveTo_earth = magnetoDCM.ToEarth(magnetometer);
-                    sum.x /= (float)count;
-                    sum.y /= (float)count;
-                    sum.z /= (float)count;
-                   // sum.x += data.Magneto.Offset.X;
-                   // sum.y += data.Magneto.Offset.Y;
-                    Vector_3 magnetometer_reltiveTo_earth = magnetoDCM.ToEarth(sum);
-                    sum = new Vector_3(0, 0, 0);
-                    count = 0;
-                    //x(1).*Data{i}(:,1)+x(2).*Data{i}(:,2)+x(3)
-                    double xAfterClib = _magClib[0] * magnetometer_reltiveTo_earth.x + _magClib[1] * magnetometer_reltiveTo_earth.y + _magClib[2];
-                    //x(4).*Data{i}(:,1)+x(5).*Data{i}(:,2)+x(6)
-                    double yAfterClib = _magClib[3] * magnetometer_reltiveTo_earth.x + _magClib[4] * magnetometer_reltiveTo_earth.y + _magClib[5];
-                    /*   if (yAfterClib > 0)
-                       {
-                           _north = (Math.PI / 2) - Math.Atan(xAfterClib / yAfterClib);
-                       }
-                       else if (magnetometer_reltiveTo_earth.y < 0)
-                       {
-                           _north = ((3 * Math.PI) / 2) - Math.Atan(xAfterClib / yAfterClib);
-                       }
-                       else if (xAfterClib < 0)
-                       {
-                           _north = Math.PI;
-                       }
-                       else
-                       {
-                           _north = 0;
-                       }*/
-                    _north = Math.Atan2(xAfterClib, yAfterClib) + Math.PI;
-                    _northDeg = (180 / Math.PI) * _north; 
-                }
-                else
-                {
-                    sum.x += data.Magneto.Rectified.X;
-                    sum.y += data.Magneto.Rectified.Y;
-                    sum.z += data.Magneto.Rectified.Z;
-                    count++;
-                }
+
                 DCM dcm = new DCM(_yaw);
                 Vector_3 velociy = new Vector_3(data.Velocity.X, data.Velocity.Y, data.Velocity.Z);
                 Vector_3 velociy_reltiveTo_earth = dcm.ToEarth(velociy);
